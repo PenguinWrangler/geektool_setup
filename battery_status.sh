5e3fc9c919955dd17aa8fb4ec7d84bc8c1610217
TK Spencer <tylerkspencer@gmail.com>
2019-10-18 14:21:54 -0500

    updated scripts to newer versions

--- battery_status.sh
+++ battery_status.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 ############## Keyboard Mouse Trackpad Battery ######################
 
 kbatt=`ioreg -c AppleDeviceManagementHIDEventService -r -l | grep -i "BatteryPercent" | awk -F"=" {'print $2'}` &&
@@ -12,21 +10,20 @@ if [ ${#mbatt} -gt 0 ]; then echo "Mouse Battery: $mbatt %"; fi;
 
 
 
-
 ################## Airpod Battery Status####################
 
-OUTPUT='Airpods'; BLUETOOTH_DEFAULTS=$(defaults read /Library/Preferences/com.apple.Bluetooth); SYSTEM_PROFILER=$(system_profiler SPBluetoothDataType 2>/dev/null)
-MAC_ADDR=$(grep -b2 "Minor Type: Headphones"<<<"${SYSTEM_PROFILER}"|awk '/Address/{print $3}')
-CONNECTED=$(grep -ia6 "${MAC_ADDR}"<<<"${SYSTEM_PROFILER}"|awk '/Connected: Yes/{print 1}')
-BLUETOOTH_DATA=$(grep -ia6 '"'"${MAC_ADDR}"'"'<<<"${BLUETOOTH_DEFAULTS}")
+OUTPUT='Airpods'; BLUETOOTH_DEFAULTS=$(defaults read /Library/Preferences/com.apple.Bluetooth); SYSTEM_PROFILER=$(system_profiler SPBluetoothDataType 2&gt;/dev/null)
+MAC_ADDR=$(grep -b2 "Minor Type: Headphones"&lt;&lt;&lt;"${SYSTEM_PROFILER}"|awk '/Address/{print $3}')
+CONNECTED=$(grep -ia6 "${MAC_ADDR}"&lt;&lt;&lt;"${SYSTEM_PROFILER}"|awk '/Connected: Yes/{print 1}')
+BLUETOOTH_DATA=$(grep -ia6 '"'"${MAC_ADDR}"'"'&lt;&lt;&lt;"${BLUETOOTH_DEFAULTS}")
 #BATTERY_LEVELS=("BatteryPercentCombined" "HeadsetBattery" "BatteryPercentSingle" "BatteryPercentCase" "BatteryPercentLeft" "BatteryPercentRight")
 BATTERY_LEVELS=(  "BatteryPercentCase" "BatteryPercentLeft" "BatteryPercentRight")
 
 
 if [[ "${CONNECTED}" ]]; then
   for I in "${BATTERY_LEVELS[@]}"; do
-    declare -x "${I}"="$(awk -v pat="${I}" '$0~pat{gsub (";",""); print $3 }'<<<"${BLUETOOTH_DATA}")"
-    [[ ! -z "${!I}" ]] && OUTPUT="${OUTPUT} $(awk '/BatteryPercent/{print substr($0,15,1)": "}'<<<"${I}")${!I}%"
+    declare -x "${I}"="$(awk -v pat="${I}" '$0~pat{gsub (";",""); print $3 }'&lt;&lt;&lt;"${BLUETOOTH_DATA}")"
+    [[ ! -z "${!I}" ]] && OUTPUT="${OUTPUT} $(awk '/BatteryPercent/{print substr($0,15,1)": "}'&lt;&lt;&lt;"${I}")${!I}%"
   done
   printf "%s\\n" "${OUTPUT}"
 else
--- ip_weather.sh
+++ ip_weather.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-ip="$(curl -s ifconfig.co)"
-
-if [ "$ip" = "4.15.43.74" ] || [ "$ip" = "24.244.111.42" ];
-	then
-		echo "Little Rock, AR" & curl --header "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X)" --silent "https://www.accuweather.com/en/us/little-rock-ar/72201/weather-forecast/31269_pc" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print substr($13,10,25)", "$10"°" }'| sed 's/"});//' | head -1
- else
-		if [[ "$ip" = *"50"* ]] || [[ "$ip" = "69.29.4.82" ]];
-			then
-				echo "Cabot, AR" & curl --header "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X)" --silent "https://www.accuweather.com/en/us/cabot-ar/72023/weather-forecast/31139_pc" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print substr($13,10,25)", "$10"°" }'| sed 's/"});//' | head -1
-	else echo "No Location Available"
-		fi
-fi
+curl wttr.in/?format="%l+%c+%C+%t+%w+%p" | sed 's/, United States /\'$'\n/g'
